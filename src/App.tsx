3f74fe270b5eaccb8d8c4687f45d82148b62b5ad
Charles Perier <charles.perier@student-cs.fr>
2022-09-21 17:39:29 +0200

    install MUI and change public info

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Ca va ou quoi ?
         </p>
         <a
           className="App-link"
