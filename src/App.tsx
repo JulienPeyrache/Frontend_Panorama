f3f7351107855d4101d051fce098c47c930440c9
Charles Perier <charles.perier@student-cs.fr>
2022-09-23 12:27:53 +0200

    add routing system

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import './App.css';
 import { Home } from './pages/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App(): React.ReactElement {
   return (
-    <Home />
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
+    </BrowserRouter>
   );
 }
 
