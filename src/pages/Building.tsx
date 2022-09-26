3ffff5e9918c1ca76ccc61029f7b636573cc0f5e
Anna Grigoriu <anna-agatha.grigoriu@student-cs.fr>
2022-09-26 21:54:27 +0200

    better lists in building page

--- src/pages/Building.tsx
+++ src/pages/Building.tsx
@@ -1,5 +1,6 @@
 import './Building.css';
-import logo from '../assets/logo_macif.png';
+import restaurant from '../assets/restaurant.png';
+import { Button } from '@mui/material';
 
 export const Building = () : React.ReactElement => {
     const building_name:string = "Bâtiment Paris Pernet"
@@ -7,8 +8,39 @@ export const Building = () : React.ReactElement => {
         <div className="building">
         <h1>Recherche</h1>
         <h2> {building_name} </h2>
-        <div className="flex-container">
-
+        <p> Description du bâtiment (horaires) </p>
+        <h3>Services Disponibles:</h3>
+        <div style={{display: "flex", justifyContent: "space-evenly", flexDirection: "row", flexWrap: "wrap", rowGap: '20px'}}>
+            <div style={{display:"flex", justifyContent:"space-evenly", flexDirection:"row", alignItems:"center"}}>
+                <img className='logoService' src={restaurant} alt="Logo Service" style={{height:60, width:60}}></img>
+                <p> Restaurant: </p>
+                <p> 12:00-14:00 </p>
+                <Button variant='contained' sx={{fontSize: 8}}> Informations </Button>
+            </div>
+            <div style={{display:"flex", justifyContent:"space-evenly", flexDirection:"row", alignItems:"center"}}>
+                <img className='logoService' src={restaurant} alt="Logo Service" style={{height:60, width:60}}></img>
+                <p> Restaurant: </p>
+                <p> 12:00-14:00 </p>
+                <Button variant='contained' sx={{fontSize: 8}}> Informations </Button>
+            </div>
+            <div style={{display:"flex", justifyContent:"space-evenly", flexDirection:"row", alignItems:"center"}}>
+                <img className='logoService' src={restaurant} alt="Logo Service" style={{height:60, width:60}}></img>
+                <p> Restaurant: </p>
+                <p> 12:00-14:00 </p>
+                <Button variant='contained' sx={{fontSize: 8}}> Informations </Button>
+            </div>
+            <div style={{display:"flex", justifyContent:"space-evenly", flexDirection:"row", alignItems:"center"}}>
+                <img className='logoService' src={restaurant} alt="Logo Service" style={{height:60, width:60}}></img>
+                <p> Restaurant: </p>
+                <p> 12:00-14:00 </p>
+                <Button variant='contained' sx={{fontSize: 8}}> Informations </Button>
+            </div>
+            <div style={{display:"flex", justifyContent:"space-evenly", flexDirection:"row", alignItems:"center"}}>
+                <img className='logoService' src={restaurant} alt="Logo Service" style={{height:60, width:60}}></img>
+                <p> Restaurant: </p>
+                <p> 12:00-14:00 </p>
+                <Button variant='contained' sx={{fontSize: 8}}> Informations </Button>
+            </div>
         </div>
         </div>
     )
