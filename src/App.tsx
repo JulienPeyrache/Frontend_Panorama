01853a9f159a442dba1229e4bdcaa73ed2a0156a
Julien Peyrache <julien.peyrache@student-cs.fr>
2022-09-28 12:17:32 +0200

    test v1

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import './App.css';
 import { Home } from './pages/Home';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AdvancedSearch } from './pages/AdvancedSearch';
+import { AdminService } from './pages/AdminService';
 
 function App(): React.ReactElement {
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/advanced-search" element={<AdvancedSearch />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/advanced-search" element={<AdvancedSearch />} />
+        <Route path="/admin-service" element={<AdminService />} />
+      </Routes>
     </BrowserRouter>
   );
 }
--- src/pages/AdminService.tsx
+++ src/pages/AdminService.tsx
@@ -3,21 +3,19 @@ import FilterBar from "../components/FilterBar"
 // import { Button } from "@mui/material"
 import logo from "../assets/logo_macif.png"
 import "./AdminService.css"
+import { useState } from "react"
 
 export const AdminService = (): React.ReactElement => {
+    const [liste, setListe] = useState([{ id: 0, nom: "Test" }]);
+    const elements = liste.map((elem) => <FilterBar label={elem.nom} />)
+
     return (
         <div className="home">
             <h1>Gestion des services</h1>
             <img className="logo" src={logo} alt="Logo MACIF" />
+            <FilterBar label='' />
             <Stack className="stack">
-                <FilterBar label="Ville" />
-                <FilterBar label="Bâtiment" />
-                <FilterBar label="Type de service" />
-                <FilterBar label="Service" />
-                <FilterBar label="Service" />
-                <FilterBar label="Service" />
-                <FilterBar label="Service" />
-                <FilterBar label="Service" />
+                {elements}
             </Stack>
         </div>
     )
