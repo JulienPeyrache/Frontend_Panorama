9570a0111886c4e59d0719a135c91816c905ec8f
Charles Perier <charles.perier@student-cs.fr>
2022-10-03 17:42:34 +0200

    feat/get equipments list and show grid

--- src/pages/ManagerEquipment.tsx
+++ src/pages/ManagerEquipment.tsx
@@ -1,5 +1,24 @@
-import { Grid } from "@mui/material";
+import Grid2 from "@mui/material/Unstable_Grid2";
+import Item from "@mui/material/Unstable_Grid2";
+import { baseURL } from "../components/Const";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export const ManagerEquipment = (): React.ReactElement => {
-    return <a> Item 1 </a>;
+
+    const [equipments, setEquipments] = useState<any[]>([]);
+
+    useEffect(() => {
+        axios
+            .get(baseURL + "/api/equipment")
+            .then((res) => setEquipments(res.data));
+    }, []);
+
+    return (
+        <Grid2 container spacing={2} sx={{color: "black",}}>
+            {equipments.map((equipment) => (<Grid2 key={equipment.id} xs="auto">
+                <Item>Coucou</Item>
+            </Grid2>))}        
+        </Grid2>
+    )
 };
