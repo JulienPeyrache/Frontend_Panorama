1d9d1009f59824b6f36a85de9ebe7e3811c1d3a6
Charles Perier <charles.perier@student-cs.fr>
2022-10-03 16:48:44 +0200

    change ManagerHome

--- src/pages/ManagerHome.tsx
+++ src/pages/ManagerHome.tsx
@@ -1,16 +1,11 @@
-// import { Stack } from "@mui/system"
-import FilterBar from "../components/FilterBar";
-// import { Button } from "@mui/material"
 import "./ManagerHome.css";
-import { Grid } from "@mui/material";
-import { useState } from "react";
-import NavTabs from "../components/NavTabs";
+import { NavTabsManager } from "../components/NavTabs";
 
 export const ManagerHome = (): React.ReactElement => {
     return (
-        <div className="manager_home">
+        <div className="manager-home">
             <h1>Gestion des services</h1>
-            <NavTabs />
+            <NavTabsManager />
         </div>
     );
 };
