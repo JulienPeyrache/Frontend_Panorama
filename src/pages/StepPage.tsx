91a3ccc313592d956d89ae6d29c575011d3fbd33
Charles Perier <charles.perier@student-cs.fr>
2022-10-17 18:29:13 +0200

    better looking page and first iteration of buildings list onClick

--- src/pages/StepPage.tsx
+++ src/pages/StepPage.tsx
@@ -1,6 +1,7 @@
 import { Grid, ThemeProvider, Typography } from "@mui/material";
 import { ReactElement } from "react";
 import { theme } from "../assets/Theme";
+import { HeaderBar } from "../components/HeaderBar";
 
 interface StepCardProps {
 	title: string;
@@ -8,29 +9,35 @@ interface StepCardProps {
 
 export const StepCard = ({ title }: StepCardProps): ReactElement => {
 	return (
-		<Grid
-			item
-			xs={12}
-			sm={6}
-			md={4}
-			sx={{
-				borderRadius: 2,
-				border: 3,
-				p: 2,
-				m: 1,
-				backgroundColor: "#90caf9",
-				justifyContent: "center",
-				alignItems: "center",
-				display: "flex",
-				flexDirection: "row",
-			}}
-		>
-			<ThemeProvider theme={theme}>
+		<ThemeProvider theme={theme}>
+			<Grid
+				item
+				xs={12}
+				sm={6}
+				md={4}
+				sx={{
+					borderRadius: 2,
+					p: 2,
+					m: 1,
+					justifyContent: "center",
+					alignItems: "center",
+					display: "flex",
+					flexDirection: "row",
+					color: "#505050",
+					transition: "0.3s",
+					boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+					"&:hover": {
+						cursor: "pointer",
+						backgroundColor: "lightgray",
+						color: "#202020",
+					},
+				}}
+			>
 				<Typography variant="h5" sx={{ textAlign: "center" }}>
 					<b>{title}</b>
 				</Typography>
-			</ThemeProvider>
-		</Grid>
+			</Grid>
+		</ThemeProvider>
 	);
 };
 
@@ -38,11 +45,21 @@ export default function StepPage(): ReactElement {
 	return (
 		<>
 			<ThemeProvider theme={theme}>
+				<HeaderBar />
+				<Typography variant="h3" align="center" sx={{ m: 2, marginTop: 4 }}>
+					<b>Commodités</b>
+				</Typography>
 				<Grid
 					container
 					padding={2}
 					sx={{ display: "flex", justifyContent: "center" }}
 				>
+					<StepCard title="Organiser un événement" />
+					<StepCard title="Réserver une salle de réunion" />
+					<StepCard title="Se rendre à la gare" />
+					<StepCard title="Manger de la soupe" />
+					<StepCard title="Réserver un panier" />
+					<StepCard title="Pondre un oeuf" />
 					<StepCard title="Organiser un événement" />
 					<StepCard title="Réserver une salle de réunion" />
 					<StepCard title="Se rendre à la gare" />
